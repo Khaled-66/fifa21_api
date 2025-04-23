31142a82fe98a5b357913287a84828d359cba689
Khaled-66 <154020876+Khaled-66@users.noreply.github.com>
2025-04-23 12:59:17 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+import os
 
 app = Flask(__name__)
+df = pd.read_csv("players21-data.csv",low_memory=False) 
+players_by_id = df.set_index("ID").to_dict(orient="index") #key: player_id, value: player data
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Hello, World!'
+    return jsonify({"message": "Welcome to the FIFA 21 API!"})
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/player/<int:player_id>", methods=["GET"])
+def get_player(player_id):
+    player = players_by_id.get(player_id)
+    if player:
+        return jsonify({"id": player_id, **player})
+    else:
+        return jsonify({"error": "Player not found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
